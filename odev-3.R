1c3daa2bca9ae2496e9118642a1efee33a4d616d
emrekayik <emrekayik1905@gmail.com>
2024-10-28 01:11:30 +0300

    3. hafta soruları

---
+++ odev-3.R
@@ -0,0 +1,81 @@
+# 2. Hafta odevi
+## Emre Kayık
+## https://github.com/emrekayik/r-ile-programlama.git
+
+# Soru 1 - Aşağıda tanımlanan x vektörünü 3 satır ve 4 sütundan oluşan,
+# bir başka deyişle (3x4) boyutlarında matrise dönüştüren R kodunu yazınız.
+
+x <- c(3,7,8,2,1,0,-1,3,8,8,6,3)
+
+# -----------------------------------------------------------------------------------------
+
+# Soru 2 - (7,3,2,1,6,3,8,3,1) elemanlarından oluşan (3x3) boyutundaki
+# matrisi yaratan R kodunu yazınız.
+
+
+# -----------------------------------------------------------------------------------------
+
+# Soru 3 - Aşağıdaki matrisi yaratan kodu yazınız.
+
+# | 5  2  7 |
+# | 8  3  0 |
+# | -2 2  6 |
+
+  
+# -----------------------------------------------------------------------------------------
+
+# Soru 4 - Aşağıdaki matrisi yaratan kodu yazınız.
+
+# | -1  4  -5 |
+# | 0   3   8 |
+
+
+
+# -----------------------------------------------------------------------------------------
+
+# Soru 5 - Aşağıdaki matrisi yarattıktan sonra, birinci satır 3. sütununun elemanını
+# 9 olarak değiştiren kodu yazınız.
+
+# | 7   1   1   3 |
+# | 2   1   2   1 |
+# | 3   0   6   7 |
+
+# -----------------------------------------------------------------------------------------
+
+# Soru 6 - Aşağıdaki matris üzerinde 3'den büyük elemanları seçerek bulan.
+# R kodunu yazınız.
+
+# |  2    5   0 |
+# |  2   -4   6 |
+# | -2    1   2 |
+
+
+# -----------------------------------------------------------------------------------------
+
+# Soru 7 - Aşağıdaki matrisin 2. sütununu silen R kodunu yazınız.
+
+# |  1    6   2 |
+# |  3    4  -6 |
+# |  5   11   4 |
+
+
+# -----------------------------------------------------------------------------------------
+
+# Soru 8 - Aşağıdaki matrisin c(3,4) vektörünü sütun olarak ekleyen R kodunu yazınız.
+
+# |  2    2   8 |
+# | 10    7  -3 |
+
+
+# -----------------------------------------------------------------------------------------
+
+# Soru 9 - Aşağıdaki matrisleri satır olarak birleştiren R kodunu yazınız.
+
+# |  2    2   8 |
+# | 10    7  -3 |
+
+
+
+# -----------------------------------------------------------------------------------------
+
+# Soru 6 - Aşağıdaki matrisi yaratan kodu yazınız.
